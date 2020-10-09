8885365df7562899784badacf9e2f374544df94d
sanazez <sanazez1234@gmail.com>
2020-10-09 22:31:52 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let array = [];
+    let brackets = Object.fromEntries(bracketsConfig);
+    let arrStr = str.split('');
+    for (let item of arrStr) {
+        if (array[0] === item) {
+            array.shift();
+        } else if (brackets[item]) {
+            array.unshift(brackets[item]);
+        } else {
+            return false;
+        }
+    }
+    if (array.length === 0) return true;
+    else return false;
 }
